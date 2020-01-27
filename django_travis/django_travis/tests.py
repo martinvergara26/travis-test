2a6946d232c53eadbf2426119c07861d17b0b08f
Martín Vergara <martinmorsevergara@gmail.com>
2020-01-27 18:11:54 -0300

    Two tests

--- django_travis/django_travis/tests.py
+++ django_travis/django_travis/tests.py
@@ -6,4 +6,6 @@ class SimpleTests(TestCase):
     def test_first_test(self):
         self.assertIs(False, False)
 
-    
+    def test_second_test(self):
+        print('On second test')
+        self.assertIs(False, False)
